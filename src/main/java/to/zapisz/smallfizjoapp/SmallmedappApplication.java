a2122f3bca074b7fdaa40a2555ac8f300baa8d4e
eMThe94 <marek.t1994@interia.pl>
2023-03-23 18:54:50 +0100

    Repo creator

---
+++ src/main/java/to/zapisz/smallfizjoapp/SmallmedappApplication.java
@@ -0,0 +1,13 @@
+package to.zapisz.smallfizjoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallmedappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallmedappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/to/zapisz/smallfizjoapp/SmallmedappApplicationTests.java
@@ -0,0 +1,13 @@
+package to.zapisz.smallfizjoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallmedappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
