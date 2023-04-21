b73b350c5736bb492f97505a4bb2f46c8ff1a76a
eMThe94 <marek.t1994@interia.pl>
2023-04-21 14:54:45 +0200

    Commit 01 21.04.2023

--- src/main/java/to/zapisz/smallfizjoapp/model/codes/ICF.java
+++ src/main/java/to/zapisz/smallfizjoapp/model/codes/ICF.java
@@ -3,8 +3,5 @@ package to.zapisz.smallfizjoapp.model.codes;
 import jakarta.persistence.Column;
 
 public class ICF {
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
+
 }
